2b65833d56d4026dd3f18815e9375cafe2f40ed3
AlanOng2004 <alancx73@gmail.com>
2024-07-09 23:03:39 +0800

    completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  var number = (temp - 32) * 5/9;
+  var rounded = Math.round(number * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  var number = temp * 9/5 + 32;
+  var rounded = Math.round(number * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
