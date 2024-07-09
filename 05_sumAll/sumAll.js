3cf2a50f0d9871999b50115352a1c11c9126dbde
AlanOng2004 <alancx73@gmail.com>
2024-07-09 22:23:26 +0800

    completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let n = 0;
+    let total = 0;
+    if (typeof(a) != "number" || typeof(b) != "number" || a < 0 || b < 0){
+        return "ERROR";
+    }
+    else{
+        if (a <= b){
+            n = b - a + 1;
+        }
+        else {
+            n = a - b + 1;
+        }
+    }
 
+    total = n * (a + b) / 2;
+    return total;
 };
 
 // Do not edit below this line
